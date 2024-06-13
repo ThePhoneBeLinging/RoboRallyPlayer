9d9cbe3ec47a4f0e6d05f2f1d2afed0e5d73f250
Elias Larsen <s235125@dtu.dk>
2024-06-13 14:53:55 +0200

    Added button for updating available lobbies

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/LobbyView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/LobbyView.java
@@ -1,9 +1,11 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
-import dk.dtu.compute.se.pisd.roborally.controller.AppController;
 import dk.dtu.compute.se.pisd.roborally.APITypes.Lobby;
+import dk.dtu.compute.se.pisd.roborally.controller.AppController;
 import javafx.application.Platform;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,80 +16,93 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 
-public class LobbyView extends VBox {
+public class LobbyView extends VBox
+{
 
-    private TextField searchBar;
-    private TextArea chatArea;
-    private VBox lobbyList;
-    private HBox hBox;
-    private AppController appController;
-    private Lobby joinedLobby;
-
+    private final TextField searchBar;
+    private final TextArea chatArea;
+    private final VBox lobbyList;
+    private final HBox hBox;
+    private final AppController appController;
     RestTemplate restTemplate = new RestTemplate();
     String url = "http://localhost:8080/lobby/getAll";
+    private Lobby joinedLobby;
 
     //List response = restTemplate.getForObject(url, List.class);
 
-    public LobbyView(AppController appController) {
+    public LobbyView(AppController appController)
+    {
         this.appController = appController;
         hBox = new HBox();
         searchBar = new TextField();
         searchBar.setPromptText("Search lobbies...");
-        searchBar.setMinSize(300,20);
+        searchBar.setMinSize(300, 20);
         lobbyList = new VBox();
         lobbyList.setMinSize(300, 500);
         chatArea = new TextArea();
         chatArea.setMinSize(300, 700);
         chatArea.setEditable(true);
+        Button updateLobbiesButton = new Button("Update Lobbies");
+        updateLobbiesButton.setOnAction(e -> fetchLobbies());
         hBox.getChildren().addAll(lobbyList, chatArea);
-        this.getChildren().addAll(searchBar, hBox);
+        this.getChildren().addAll(searchBar, hBox, updateLobbiesButton);
         joinedLobby = null;
         fetchLobbies();
     }
 
-
-    public void joinLobbyView() {
-        System.out.println("Joined Lobby View");
-        while (this.joinedLobby == null)
-        {
+    private void fetchLobbies()
+    {
+        new Thread(() -> {
             try
             {
-                Thread.currentThread().sleep(1000);
-            }
-            catch (InterruptedException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
-        this.appController.joinLobby(joinedLobby);
-    }
-
-    private void fetchLobbies() {
-        new Thread(() -> {
-            try {
-                ResponseEntity<List<Long>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Long>>() {
+                ResponseEntity<List<Long>> response = restTemplate.exchange(url, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<Long>>()
+                {
                 });
                 List<Long> lobbies = response.getBody();
 
                 Platform.runLater(() -> populateVBox(lobbies));
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Platform.runLater(() -> chatArea.setText("Failed to fetch lobbies: " + e.getMessage()));
             }
 
         }).start();
     }
 
-    private void joinLobbies(Long lobbyID) {
+    private void populateVBox(List<Long> lobbies)
+    {
+        lobbyList.getChildren().clear();
+
+        for (Long lobbyID : lobbies)
+        {
+            Button label = new Button("Lobby ID: " + lobbyID);
+            label.setMinSize(300, 20);
+            label.setOnAction(e -> {
+                joinLobbies(lobbyID);
+            });
+            lobbyList.getChildren().add(label);
+        }
+    }
+
+    private void joinLobbies(Long lobbyID)
+    {
         String lobbyUrl = "http://localhost:8080/lobby/join?gameID=" + lobbyID;
 
         new Thread(() -> {
-            try {
-                ResponseEntity<Lobby> response = restTemplate.exchange(lobbyUrl, HttpMethod.GET, null, new ParameterizedTypeReference<Lobby>() {
+            try
+            {
+                ResponseEntity<Lobby> response = restTemplate.exchange(lobbyUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<Lobby>()
+                {
                 });
                 this.joinedLobby = response.getBody();
                 chatArea.appendText("Joined Lobby: " + this.joinedLobby.getGameID() + "\n");
 
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Platform.runLater(() -> chatArea.setText("Failed to join lobbies: " + e.getMessage()));
             }
 
@@ -95,16 +110,20 @@ public class LobbyView extends VBox {
         joinLobbyView();
     }
 
-    private void populateVBox(List<Long> lobbies) {
-        lobbyList.getChildren().clear();
-
-        for(Long lobbyID: lobbies) {
-            Button label = new Button("Lobby ID: " + lobbyID);
-            label.setMinSize(300,20);
-            label.setOnAction(e -> {
-                joinLobbies(lobbyID);
-            });
-            lobbyList.getChildren().add(label);
+    public void joinLobbyView()
+    {
+        System.out.println("Joined Lobby View");
+        while (this.joinedLobby == null)
+        {
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                throw new RuntimeException(e);
+            }
         }
+        this.appController.joinLobby(joinedLobby);
     }
 }
