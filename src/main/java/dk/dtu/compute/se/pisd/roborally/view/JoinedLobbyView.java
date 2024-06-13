317b118d0ac6da24af48f8cd150dd72c0e00bc04
Elias Larsen <s235125@dtu.dk>
2024-06-13 22:49:20 +0200

    Changed tabName to always be Player

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/JoinedLobbyView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/JoinedLobbyView.java
@@ -102,7 +102,7 @@ public class JoinedLobbyView extends HBox
         GameController gameController = new GameController(board);
         for (int i = 0; i < this.listOfPlayers.size(); i++)
         {
-            board.addPlayer(new Player(board, "Player" + i, gameController.moveController));
+            board.addPlayer(new Player(board, "Player", gameController.moveController));
             board.getPlayer(i).setPlayerID(this.listOfPlayers.get(i));
             board.getPlayer(i).setSpace(board.getSpace(i, i));
         }
