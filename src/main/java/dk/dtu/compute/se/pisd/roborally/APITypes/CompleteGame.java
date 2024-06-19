a4831eab2ba74e38febc2e22883d6e5035da73eb
Elias Larsen <s235125@dtu.dk>
2024-06-19 12:21:45 +0200

    UpgradeCards are now loaded by the player

--- src/main/java/dk/dtu/compute/se/pisd/roborally/APITypes/CompleteGame.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/APITypes/CompleteGame.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.tomcat.util.http.parser.Upgrade;
 
 import java.util.List;
 
@@ -23,5 +22,5 @@ public class CompleteGame
     private List<EnergyCube> energyCubes;
     private List<Player> playerList;
     private List<Card> cards;
-    private List<Upgrade> upgradeList;
+    private List<UpgradeCard> upgradeCards;
 }
