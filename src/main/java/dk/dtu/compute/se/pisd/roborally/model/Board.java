b5e29e4aee931df419226ea3dbd913d85fd35978
Elias Larsen <s235125@dtu.dk>
2024-06-21 17:50:23 +0200

    .

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -250,9 +250,10 @@ public class Board extends Subject
                         if (card.getLocation().equals("OPTION") && card.getPlayerID() == this.getPlayerID())
                         {
                             this.options.add(card.getCommand());
+                            gameBoardPlayer.notify();
                         }
                     }
-                    gameBoardPlayer.notify();
+
                 }
             }
         }
